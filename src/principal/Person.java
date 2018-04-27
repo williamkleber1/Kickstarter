5582410daf7577bd9693562b21fb41003564e0e8
william kleber <wkas@ic.ufal.br>
2018-04-27 14:53:36 -0300

    criando classes

--- src/principal/Person.java
+++ src/principal/Person.java
@@ -1,17 +1,58 @@
 package principal;
 
+import java.util.ArrayList;
+
 public class Person {
-
-	public Person() {
-		// TODO Auto-generated constructor stub
+	
+	private String name;
+	private String email;
+	private String Senha;
+	public ArrayList<Project> projects = new ArrayList<Project>();
+	
+	public Person(String name, String email, String senha) {
+		super();
+		this.name = name;
+		this.email = email;
+		this.Senha = senha;
 	}
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public String getName() {
+		return name;
 	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getSenha() {
+		return Senha;
+	}
+
+	public void setSenha(String senha) {
+		Senha = senha;
+	}
+	
+	public void insertProject( Project proj) {
+		this.projects.add(proj);
+		
+	}
+	
+	public void remove_project(Project proj) {
+		for(int i = 0; i < this.projects.size(); i++)
+		{
+			
+		}
+		
+	}
+
+
 
 }
---
+++ src/principal/Project.java
@@ -0,0 +1,42 @@
+package principal;
+
+public class Project {
+
+	private int value_needed;
+	private int current_value;
+	private String description;
+	
+	public Project(int value_needed, int current_value, String description) {
+		super();
+		this.value_needed = value_needed;
+		this.current_value = current_value;
+		this.description = description;
+	}
+
+	public int getValue_needed() {
+		return value_needed;
+	}
+
+	public void setValue_needed(int value_needed) {
+		this.value_needed = value_needed;
+	}
+
+	public int getCurrent_value() {
+		return current_value;
+	}
+
+	public void setCurrent_value(int current_value) {
+		this.current_value = current_value;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+	
+
+}
